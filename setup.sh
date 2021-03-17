8a2ddf30addd89856e7b8e32cccecc2408c87313
PANCA PUTRA PAHLAWAN <rectapanca@gmail.com>
2021-03-17 15:11:59 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,13 @@
 
 #author Panca Putra Pahlawan
 
-USER_SERVER=
-SECRET=
+while getopts u:s: flag
+do
+    case "${flag}" in
+        u) USER_SERVER=${OPTARG};;
+        s) SECRET=${OPTARG};;
+    esac
+done
 
 sshpass -p $SECRET ssh -o StrictHostKeyChecking=no $USER_SERVER "echo "$SECRET" | sudo -S wget https://raw.githubusercontent.com/EPX-PANCA/K8S/main/auto-setup-docker.sh"
 sshpass -p $SECRET ssh -o StrictHostKeyChecking=no $USER_SERVER "echo "$SECRET" | sudo -S chmod 600 auto-setup-docker.sh"
