e20617f1b586e072ea19583bc637cd4e018270a7
PANCA PUTRA PAHLAWAN <rectapanca@gmail.com>
2021-03-18 08:43:34 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@
 
 #author Panca Putra Pahlawan
 
-echo "---------------------------------------------------------------------------------------------------"
-echo "| Auto Script Docker for Kubernetes v.0.2-alpha | https://github.com/EPX-PANCA/docker-auto-script |"
-echo "---------------------------------------------------------------------------------------------------"
+echo "---------------------------------------------------"
+echo "|  Auto Script Docker for Kubernetes v.0.2-alpha  |"
+echo "| https://github.com/EPX-PANCA/docker-auto-script |"
+echo "---------------------------------------------------"
 
 while getopts u:p: flag; do
     case "${flag}" in
@@ -21,10 +22,11 @@ then
 fi
 
 MYNAME=$(sshpass -p $SECRET ssh -o StrictHostKeyChecking=no $USER_SERVER "whoami");
-
-sshpass -p $SECRET ssh -o StrictHostKeyChecking=no $USER_SERVER "echo "$SECRET" | sudo -S wget -N https://raw.githubusercontent.com/EPX-PANCA/docker-auto-script/main/auto-docker.sh"
+echo "Download Installer"
+sshpass -p $SECRET ssh -o StrictHostKeyChecking=no $USER_SERVER "echo "$SECRET" | sudo -S wget -N https://raw.githubusercontent.com/EPX-PANCA/docker-auto-script/main/auto-docker.sh" > /dev/null 2>&1
 sshpass -p $SECRET ssh -o StrictHostKeyChecking=no $USER_SERVER "echo "$SECRET" | sudo -S chmod +x auto-docker.sh"
 sshpass -p $SECRET ssh -o StrictHostKeyChecking=no $USER_SERVER "echo "$SECRET" | sudo -S ./auto-docker.sh"
 sshpass -p $SECRET ssh -o StrictHostKeyChecking=no $USER_SERVER "echo "$SECRET" | sudo -S usermod -aG docker $MYNAME"
+sshpass -p $SECRET ssh -o StrictHostKeyChecking=no $USER_SERVER "echo "$SECRET" | sudo -S rm auto-docker.sh" > /dev/null 2>&1
 echo "Server Reboot"
 sshpass -p $SECRET ssh -o StrictHostKeyChecking=no $USER_SERVER "echo "$SECRET" | sudo -S reboot"
